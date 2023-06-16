1de2de7d28d29ede78687bbded89844900050e0e
Aditya Kumar Rout <routadu@gmail.com>
2023-06-16 13:56:28 +0530

    Added more comments in js-assesment.js

--- assessment/js-assesment.js
+++ assessment/js-assesment.js
@@ -38,17 +38,22 @@ function listNFTs(){
     }
 }
 
+// function to print and return the number of NFTs stored in the list
 function getTotalSupply(){
     console.log("Total NFTs: "+container.length)
     return container.length
 }
 
+// calling mintNFT function with few random data
 mintNFT(1000,"JS Proof Tutorial",10,9089,"jehf48t74hfuij")
 mintNFT(1001,"ETH Proof Tutorial",16,9089,"mnvfhf4be34hfuij")
 mintNFT(1002,"AI Proof Tutorial",21,9089,"sahf48t74hfuij")
 mintNFT(1003,"AVAX Proof Tutorial",6,9089,"pouf48ttehfuij")
 mintNFT(1004,"Poly Proof Tutorial",9,9089,"nbdrr48t74hfuij")
 
+// calling listNFTs function to print the NFT collection
 listNFTs()
 console.log()
+
+// calling getTotalSupply function to print the number of NFTs available in the list
 getTotalSupply()
